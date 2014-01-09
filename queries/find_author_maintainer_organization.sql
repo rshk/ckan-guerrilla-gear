b899af4a21a7c3a843d8a7dfbce81466e0e1a935
Samuele Santi <redshadow@hackzine.org>
2014-01-09 18:00:29 +0100

    Added query to list author/maintainer/organization associated to packages

---
+++ queries/find_author_maintainer_organization.sql
@@ -0,0 +1,9 @@
+﻿--select distinct "key" from package_extra order by "key"
+--select distinct value from package_extra where key='Titolare'
+--select maintainer, count(*) from package group by maintainer order by count desc
+
+select distinct p.author, p.maintainer, g.name
+from package p, "member" m, "group" g
+where m.table_name = 'package'
+and m.table_id = p.id
+and g.id = m.group_id
